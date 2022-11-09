6a9c3734c1c794f738fc5baf9dd3dba2b599a356
Md. Shehab Chowdhury <shehab15-11396@diu.edu.bd>
2022-11-09 06:06:10 +0600

    navbar done

---
+++ src/components/header/Header.js
@@ -0,0 +1,56 @@
+import React from "react";
+import "./Header.css";
+const Header = () => {
+  return (
+    <div>
+      <div className="navbar bg">
+        <div className="flex-1">
+          <a className="btn btn-ghost normal-case text-xl w-24	">
+            <img
+              src="https://res.cloudinary.com/dc9bjecdl/image/upload/v1667949925/Assignment%2011/beach-tour-logo-4505456896-seeklogo.com_hnehgp.png"
+              alt="logo"
+            ></img>
+          </a>
+        </div>
+        <div className="flex-none">
+          <div>
+            <p className="border p-2 mr-3 cursor-pointer border-red-900">
+              Add Services
+            </p>
+          </div>
+          <div>
+            <p className="border p-2  mr-3 cursor-pointer border-red-900">
+              My Review
+            </p>
+          </div>
+          <div
+            className="dropdown dropdown-end tooltip  tooltip-bottom"
+            data-tip="Log Out"
+          >
+            <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
+              <div className="w-10 rounded-full  ">
+                <img src="https://placeimg.com/80/80/people" />
+              </div>
+            </label>
+            <ul
+              tabIndex={0}
+              className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
+            >
+              <li>
+                <a className="justify-between">Log In</a>
+              </li>
+              <li>
+                <a>Sign In</a>
+              </li>
+              <li>
+                <a>Logout</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
---
+++ src/routes/Routes.js
@@ -0,0 +1,62 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "../components/home/Home";
+import Main from "../layout/Main";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Main></Main>,
+
+    children: [
+      {
+        path: "/",
+        element: <Home></Home>,
+      },
+      //   {
+      //     path: "/login",
+      //     element: <Login></Login>,
+      //   },
+
+      //   {
+      //     path: "/signup",
+      //     element: <Signup></Signup>,
+      //   },
+      //   {
+      //     path: "/courses",
+      //     element: <Courses></Courses>,
+      //   },
+      //   {
+      //     path: "/faq",
+      //     element: <Faq></Faq>,
+      //   },
+      //   {
+      //     path: "/blogs",
+      //     element: <Blogs></Blogs>,
+      //   },
+      //   {
+      //     path: "/purchase",
+      //     element: <Success></Success>,
+      //   },
+      //   {
+      //     path: "/cart",
+      //     element: (
+      //       <PrivateRoute>
+      //         <Cart></Cart>
+      //       </PrivateRoute>
+      //     ),
+      //   },
+      //   {
+      //     path: "/category/:id",
+      //     element: <Card></Card>,
+      //     loader: ({ params }) =>
+      //       fetch(`https://brins-server.vercel.app/category/${params.id}`),
+      //   },
+      {
+        path: "*",
+        element: <h1 className="text-6xl text-center m-20">404 Not Found</h1>,
+      },
+    ],
+  },
+]);
+
+export default router;
