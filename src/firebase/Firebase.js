834774b1fdf1fbf14bcd379572c7f89eef805331
Md. Shehab Chowdhury <shehab15-11396@diu.edu.bd>
2022-12-02 07:19:55 +0600

    env added  commit

--- src/firebase/Firebase.js
+++ src/firebase/Firebase.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDiv84y08sTaNyUuFEuI2Foafgl6IkJQDY",
-  authDomain: "travel-site-1b8e0.firebaseapp.com",
-  projectId: "travel-site-1b8e0",
-  storageBucket: "travel-site-1b8e0.appspot.com",
-  messagingSenderId: "481700238059",
-  appId: "1:481700238059:web:3960fdf7124849ce1d64ae",
+  apiKey: process.env.REACT_APP_apiKey,
+  authDomain: process.env.REACT_APP_authDomain,
+  projectId: process.env.REACT_APP_projectId,
+  storageBucket: process.env.REACT_APP_storageBucket,
+  messagingSenderId: process.env.REACT_APP_messagingSenderId,
+  appId: process.env.REACT_APP_appId,
 };
 
 // Initialize Firebase
